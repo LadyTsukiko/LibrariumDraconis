0ab7746100eb4adda64a0f4e74e53ce49062db97
LadyTukiko <a.degroof@yahoo.de>
2017-01-03 00:42:59 +0100

    pages

--- view/templates/main.php
+++ view/templates/main.php
@@ -4,7 +4,7 @@
 <head>
 	<meta charset="UTF-8">
 	<title><?php echo $title;?></title>
-	<link rel="stylesheet" href="css/stylesheet.css">
+	<link rel="stylesheet" href="assets/css/stylesheet.css">
 	<link href="https://fonts.googleapis.com/css?family=Aladin|Astloch:700|Berkshire+Swash|Cinzel+Decorative:400,900|Homemade+Apple|IM+Fell+Double+Pica|Lily+Script+One|Milonga|Montez|Norican" rel="stylesheet">
 </head>
 <body >
@@ -20,12 +20,16 @@
 	</aside>
 	<article class="main">
 		<?php
-		$sql = "SELECT ISBN, name, autor FROM books";
+		$datatable = "books";
+		$results_per_page = 20;
+		if (isset($_GET["page"])) { $page  = $_GET["page"]; } else { $page=1; };
+		$start_from = ($page-1) * $results_per_page;
+		$sql = "SELECT ISBN, bookLabel, authorLabel, genre_label, ID FROM $datatable ORDER BY ID ASC LIMIT $start_from,$results_per_page";
 		$result = DB::doQuery($sql);
 		if ($result->num_rows > 0) {
 		// output data of each row
 		while($row = $result->fetch_assoc()) {
-			echo "<table><tr><th>ISBN</th><th>Name</th><th>author</th><th>genre</th></tr>";
+			echo "<table><tr><th>ISBN</th><th>name</th><th>author</th><th>genre</th></tr>";
 			// output data of each row
 			while($row = $result->fetch_assoc()) {
 				echo "<tr><td>".$row["ISBN"]."</td><td>".$row["bookLabel"]."</td><td>".$row["authorLabel"]."</td><td> ".$row["genre_label"]."</td></tr>";
@@ -34,6 +38,17 @@
 		}}else {
 		echo "0 results";
 		} ?>
+		<?php
+		$sql = "SELECT COUNT(ID) AS total FROM $datatable";
+		$result = DB::doQuery($sql);
+		$row = $result->fetch_assoc();
+		$total_pages = ceil($row["total"] / $results_per_page); // calculate total pages with results
+		for ($i=1; $i<=$total_pages; $i++) {  // print links for all pages
+			echo "<a href='index.php?page=$i'";
+			if ($i==$page)  echo " class='curPage'";
+			echo ">$i</a> ";
+		};
+		?>
 	</article>
 
 	<aside class="aside info">flex item 3</aside>
